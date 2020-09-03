985d9d37eeed99f26cbea16ca9dfe79e6caf81b2
Cristhian De Castro <ccastro@ccm.local>
2020-09-03 12:02:29 -0400

    Add project files.

---
+++ NumeroParOImpar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumeroParOImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite un Numero");
+            string num1 = Console.ReadLine();
+
+            int num = Convert.ToInt32(num1);
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("El numero {0} es par.", num);
+            }
+            else {
+                Console.WriteLine("El numero {0} es impar.",num);
+            }
+        }
+    }
+}
